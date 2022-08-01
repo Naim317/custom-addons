9052904812dbc8fe47a64ac380a43fb34f86cc95
Md. Jannat UL Naim <72838150+Naim317@users.noreply.github.com>
2022-08-01 10:51:54 +0600

    first commit

---
+++ test/models/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Test(models.Model):
+    _name = 'test'
+    _description = "Test Table"
+
+    name = fields.Char(string="Enter your Name :")
+    email = fields.Char(string="Email :")
+    employeeId = fields.Char(string="Employee Id :")
+    address = fields.Char(string="Address :")
+    phone = fields.Char(string="Phone :")
+    designation = fields.Char(string="Designation :")
